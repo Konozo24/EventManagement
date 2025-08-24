95b2b1a47e4b870f76de51f2df59e4616ba3ee3e
Konozo24 <mingwei12242016@gmail.com>
2025-08-24 15:56:47 +0800

    Create Constants.h

---
+++ Event-Management/Headers/Constants.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+const string VENUES_FILE = "venues.txt";
+const string EVENTS_FILE = "events.txt";
+const string GUESTS_FILE = "guests.txt";
+const string CHECKIN_LOG_FILE = "checkin_log.txt";   
+const string REGISTRATION_FILE = "registration.txt";
+
+#endif
