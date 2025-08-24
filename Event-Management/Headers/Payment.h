96afaee8021cb114c4319b0e44fae65bdfe9158b
WChing06 <weiching06@gmail.com>
2025-08-24 23:36:15 +0800

    Update Payment.h

--- Event-Management/Headers/Payment.h
+++ Event-Management/Headers/Payment.h
@@ -4,27 +4,17 @@
 
 #include <string>
 #include <vector>
-#include "Guest.h"       // To use Guest info
-#include "Constants.h"   // For RECEIPT_FILE constant
+using namespace std;
 
 struct Product {
-    std::string name;
+    string name;
     double price;
     int quantity;
 };
 
-// Core payment functions
-void userCheckout(const std::string& name, const std::string& eventName, 
-                  int ticketAmount, double ticketPrice, 
-                  const std::vector<Product>& selectedProducts);
-
+// Payment functions
+void userCheckout(const string& name, const string& eventName, int ticketAmount, double ticketPrice,
+                  const vector<Product>& selectedProducts);
 void viewReceipts();
-
-// Helpers
-std::string buildProductSummary(const std::vector<Product>& selectedProducts, double& totalProductPrice);
-int getNextReceiptNumber();
-void saveReceipt(const std::string& name, const std::string& eventName,
-                 const std::string& productSummary, int ticketAmount, 
-                 double ticketPrice, double productPrice, const std::string& method);
 
 #endif
