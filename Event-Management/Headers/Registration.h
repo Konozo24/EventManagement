56430238139857bcd862191bd9fcd4b7959f6676
Konozo24 <mingwei12242016@gmail.com>
2025-08-29 22:22:24 +0800

    Update Registration.h

--- Event-Management/Headers/Registration.h
+++ Event-Management/Headers/Registration.h
@@ -6,14 +6,18 @@
 using namespace std;
 
 struct Registration {
-    int registrationID;
-    string registrationName;
-    int ticketAmount;       // tickets available
-    double registrationCost;
-    vector<int> guestIDs;   // store all guest IDs for this event
+    string registrationID;   // e.g. "R1", "R2"
+    string eventID;          // link to Event (e.g. "E1")
+    string eventName;
+	string eventDate;   
+    string userName;         // who registered
+    int ticketsBought;       // number of tickets bought
+	double registrationCost; // total cost
+    
+
 
     Registration();
-    Registration(int regId, const string& regName, int ticAmt, double regCost);
+    Registration(const string& regId, const string& eventId, const string& eventName, const string& eventDate, const string& userName, int tickets, double registrationCost);
 };
 
 extern vector<Registration> registrations;
@@ -26,7 +30,8 @@ void saveRegistrationToFile();
 // Display
 void displayRegistration();
 
-// Validation
-Registration* findRegistrationByName(const string& name);
+string generateRegistrationID();
+
+
 
 #endif
