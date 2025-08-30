90ac55b4b546bc12a2e5794c1cbd6c39ed9186d9
Stevenfoo061001 <stevenfqh-wm24@student.tarc.edu.my>
2025-08-31 02:30:12 +0800

    Update Report.h

--- Event-Management/Headers/Report.h
+++ Event-Management/Headers/Report.h
@@ -18,9 +18,9 @@ class Report {
 private:
     vector<BookingSummary> summaries;
 
-    void loadRegistration(const string& filename);
-    void loadGuests(const string& filename);
-    void loadVenues(const string& filename);
+    void loadRegistration(const string &filename);
+    void loadGuests(const string &filename);
+    void loadVenues(const string &filename);
 
 public:
     void generateReport();
