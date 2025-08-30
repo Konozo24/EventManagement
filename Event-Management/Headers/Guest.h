d1f44c8a37a94e047967a16dcba4cb68da6b23cb
Konozo24 <mingwei12242016@gmail.com>
2025-08-30 15:52:37 +0800

    Update Guest.h

--- Event-Management/Headers/Guest.h
+++ Event-Management/Headers/Guest.h
@@ -7,14 +7,14 @@
 using namespace std;
 
 struct Guest {
-    int guestID;
+    string guestID;
     string name;
     string eventName;
     bool checkedIn;
     string checkInTime;
 
     Guest();
-    Guest(int id, const string& guestName, const string& event);
+    Guest(const string& id, const string& guestName, const string& event);
 
     void checkIn();
 };
@@ -24,10 +24,12 @@ extern vector<Guest> guests;
 
 // Guest utilities
 void loadGuestsFromFile();
+void saveGuestsToFile();
 void displayRegisteredGuests();
-int validateGuestIDInput();
+string validateGuestIDInput();
+string generateGuestID();
 
 // Extra utilities (for Payment & Feedback)
-bool verifyGuest(int guestID, const string& name);
+bool verifyGuest(const string& guestID, const string& name);
 
 #endif
