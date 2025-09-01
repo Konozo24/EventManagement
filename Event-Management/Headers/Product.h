711065206ee548c66e68d765b1f3f35ceb2d689b
Konozo24 <mingwei12242016@gmail.com>
2025-09-01 22:23:42 +0800

    No need Product.h

--- Event-Management/Headers/Product.h
+++ Event-Management/Headers/Product.h
@@ -1,14 +1,1 @@
-#pragma once
-#ifndef PRODUCT_H
-#define PRODUCT_H
 
-#include <string>
-using namespace std;
-
-struct Product {
-    string name;
-    double price;
-    int quantity;
-};
-
-#endif
