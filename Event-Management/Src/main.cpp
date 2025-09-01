ea2ac001785c7692e8dd4bcab3eddcc221a146d0
Konozo24 <mingwei12242016@gmail.com>
2025-09-01 22:21:10 +0800

    Update main.cpp

--- Event-Management/Src/main.cpp
+++ Event-Management/Src/main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 
 int main() {
-    initializeDefaultVenues();
+    loadVenuesFromFile();
 
     cout << "=" << string(60, '=') << endl;
     cout << "EVENT MANAGEMENT MODULES" << endl;
