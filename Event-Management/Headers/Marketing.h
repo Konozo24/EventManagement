58db95212931abc86c9293d0c2a43afc35c0fcff
Konozo24 <mingwei12242016@gmail.com>
2025-09-01 22:21:45 +0800

    Update Marketing.h

--- Event-Management/Headers/Marketing.h
+++ Event-Management/Headers/Marketing.h
@@ -1,8 +1,26 @@
 #pragma once
 #ifndef MARKETING_H
 #define MARKETING_H
+#include <vector>
+#include "Event.h"       
+#include "Product.h"
+using namespace std;
+
+struct MarketingItem {
+    string eventID;
+    string name;
+    double price;
+    int quantity;
+};
+
+extern vector<MarketingItem> products;
 
 void marketingUser();
 void marketingAdmin();
+vector<MarketingItem> selectProductsForEvent(const Event& ev);
+void loadProductsFromFile();
+void saveProductsToFile();
+
+
 
 #endif
