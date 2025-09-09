68f35966b8a5c4894b2a41490388d7bc23ef6b5e
Stevenfoo061001 <stevenfqh-wm24@student.tarc.edu.my>
2025-09-09 22:41:59 +0800

    Update Login.h

--- Event-Management/Headers/Login.h
+++ Event-Management/Headers/Login.h
@@ -2,14 +2,25 @@
 #define LOGIN_H
 
 #include <string>
+#include <vector>
 using namespace std;
 
-extern bool isAdminLoggedIn;   // Global login status
-extern string currentAdmin;    // Track who is logged in
 
-bool adminLogin();             // Perform login
-void adminLogout();            // Logout function
-bool requireAdminLogin();      // Check before accessing admin features
-void adminRegister();          // Register new admin
+struct Admin {
+    string username;
+    string password;
+};
+
+
+struct AdminSession {
+    bool loggedIn = false;
+    string currentAdmin = "";
+};
+
+
+void adminRegister();
+bool adminLogin(AdminSession& session);
+void adminLogout(AdminSession& session);
+bool requireAdminLogin(AdminSession& session);
 
 #endif
